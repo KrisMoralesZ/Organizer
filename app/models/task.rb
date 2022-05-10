f41bab51612fd96cebfec0c6bfc18ef98790cd39
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-05-09 17:14:21 -0700

    model, tasks and migrations

--- app/models/task.rb
+++ app/models/task.rb
@@ -12,4 +12,17 @@
 #
 class Task < ApplicationRecord
   belongs_to :category
+  belongs_to :owner, class_name: 'User'
+  has_many :participating_users, class_name: 'Participant'
+  has_many :participants, through: :participating_users, source: :user
+  validates :participating_users, presence: true
+  validates :name, :description, presence: true
+  validates :name, uniqueness: {case_insensitive: false}
+  validate :due_date_validity
+
+    def due_date_validity
+        return if due_date.blanck?
+        return if due_date > Date.today
+        errors.add :due_date, I18n.t('task.error.invalid_due_date')
+    end
 end
